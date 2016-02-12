2827c4acab76e1ee4ef5d10e72eb931fc60a445c
Aleksa <rakicaleksa@ymail.com>
2016-02-12 15:15:13 +0100

    tweeting quotes, styling, finished

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 #Random quotes generator.
-[Try it here](http://htmlpreview.github.io/?https://github.com/aleksarakic/quotes/blob/master/index.html) 
+[See it here](http://codepen.io/superaki/pen/WrLBKB) 
--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+var quot = "";
+function getQuoteText(){
+	quot = $('.quote').text();
+}
+
 var func = function(){
 	function reqListener() {
 		  $('.quote').empty();
 			$('.author').empty();
-			var xml = this.responseText;
-			xmlDoc = $.parseXML(xml)
-		  $xml = $(xmlDoc)
-		  $quoteText = $xml.find("quote");
-		  $quoteAuthor = $xml.find("author");
-		  $(".quote").append($quoteText.text());
-		  $(".author").append($quoteAuthor.text());
-		}
+				var xml = this.responseText;
+				xmlDoc = $.parseXML(xml)
+			  $xml = $(xmlDoc)
+			  $quoteText = $xml.find("quote");
+			  $quoteAuthor = $xml.find("author");
+			  $(".quote").append($quoteText.text());
+			  $(".author").append($quoteAuthor.text());
+			  getQuoteText();
+			  $('.twitter-share-button').attr('href', "https://twitter.com/intent/tweet?text=" + quot);
+			}
 
-	var oReq = new XMLHttpRequest();
-	oReq.addEventListener("load", reqListener);
-	oReq.open("GET", "http://www.stands4.com/services/v2/quotes.php?uid=4768&tokenid=2iHYphk3qdY4rTug");
-	oReq.send();
-}//ULEPSATI!!!
+		var oReq = new XMLHttpRequest();
+		oReq.addEventListener("load", reqListener);
+		oReq.open("GET", "http://www.stands4.com/services/v2/quotes.php?uid=4768&tokenid=2iHYphk3qdY4rTug");
+		oReq.send(); 
+}
 
 $(document).ready(function(){
 	func();
+	getQuoteText();
 });
-
